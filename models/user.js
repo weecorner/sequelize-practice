aa73bfc8ff929367116bdb1afda1a5b553943e79
Geoff Bass <geoff.c.bass@gmail.com>
2017-05-05 17:07:58 -0400

    clarify comments in user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice');
 
 const User = db.define('user', {
-// YOUR CODE HERE
+// YOUR CODE HERE...
+}, {
+// ...AND HERE
 });
 
 module.exports = User;
