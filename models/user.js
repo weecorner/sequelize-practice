60dfb51301f538a1d75f619c081eff8d996b8fa1
Jing Wang <jwang2048@gmail.com>
2017-05-07 23:36:39 -0400

    completed

--- models/user.js
+++ models/user.js
@@ -3,8 +3,54 @@ const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { loggi
 
 const User = db.define('user', {
 // YOUR CODE HERE...
+	first: {
+		type: Sequelize.STRING,
+		// allowNull: false
+
+	},
+	last: {
+		type: Sequelize.STRING,
+		// allowNull: false
+	},
+	age: {
+		type: Sequelize.INTEGER,
+		validate: {
+			min: 18
+		}
+		
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		isEmail: true
+	},
+	bio: {
+		type: Sequelize.TEXT
+	}
 }, {
 // ...AND HERE
+	getterMethods: {
+		fullName: function(){
+			return `${this.first} ${this.last}`;
+		}
+	},
+	// instanceMethods: {
+	// 	haveBirthday: function(){
+	// 		return this.update({
+	// 			age: this.age + 1
+	// 		})
+	// 		.catch()
+	// 	}
+	// }
+	instanceMethods: {
+		haveBirthday: function(){
+			return this.update({
+				age: this.age + 1
+			})
+			.catch()
+		}
+	}
+
 });
 
 module.exports = User;
